b09766411baa9121640e22d8f1c237b337e39367
dlojea <dlojea@esei.uvigo.es>
2019-09-27 14:50:52 +0200

    Supera HTTPRequestGETParametersTest

--- src/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
+++ src/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
@@ -1,56 +1,97 @@
 package es.uvigo.esei.dai.hybridserver.http;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class HTTPRequest {
+	
+	private HTTPRequestMethod method;
+	private String resourceChain;
+	private String [] resourcePath;
+	private String resourceName;
+	private Map<String, String> resourceParameters = new LinkedHashMap<>();
+	private String httpVersion;
+	private Map<String, String> headerParameters = new LinkedHashMap<>();
+	private String content;
+	private int contentLength;
+	
 	public HTTPRequest(Reader reader) throws IOException, HTTPParseException {
+		BufferedReader buffer = new BufferedReader(reader);
+		
+		String line = buffer.readLine();
+		String [] field = line.split(" ");
+		
+		this.method = HTTPRequestMethod.valueOf(field[0]);
+		this.resourceChain = field[1];
+		this.httpVersion = field[2];
+		
+		String [] resource = this.resourceChain.split("\\?");
+		this.resourceName = resource[0].substring(1);
+		this.resourcePath = this.resourceName.split("/");
+		
+		String [] resourceParameters = resource[1].split("&");
+		
+		for (int i = 0; i < resourceParameters.length; i++) {
+			String [] parameter = resourceParameters[i].split("=");
+			
+			String key = parameter[0];
+			String value = parameter[1];
+			
+			this.resourceParameters.put(key, value);
+		}
+		
+		while (!(line = buffer.readLine()).equals("")) {
+			
+			String [] headerParameters = line.split(":");
+			String key = headerParameters[0].trim();
+			String value = headerParameters[1].trim();
+			
+			this.headerParameters.put(key, value);
+			
+			System.out.print(key + ": " + value);
+			
+		}
+		
 	}
 
 	public HTTPRequestMethod getMethod() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.method;
 	}
 
 	public String getResourceChain() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.resourceChain;
 	}
 
 	public String[] getResourcePath() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.resourcePath;
 	}
 
 	public String getResourceName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.resourceName;
 	}
 
 	public Map<String, String> getResourceParameters() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.resourceParameters;
 	}
 
 	public String getHttpVersion() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.httpVersion;
 	}
 
 	public Map<String, String> getHeaderParameters() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.headerParameters;
 	}
 
 	public String getContent() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.content;
 	}
 
 	public int getContentLength() {
-		// TODO Auto-generated method stub
-		return -1;
+		return this.contentLength;
 	}
 
 	@Override
