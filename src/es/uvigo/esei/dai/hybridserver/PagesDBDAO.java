5352133f13794c8736e47abb378e5e9f3d780e1b
dlojea <dlojea@esei.uvigo.es>
2019-10-14 12:56:31 +0200

    Añadido PagesDBDAO

---
+++ src/es/uvigo/esei/dai/hybridserver/PagesDBDAO.java
@@ -0,0 +1,35 @@
+package es.uvigo.esei.dai.hybridserver;
+
+public class PagesDBDAO implements PagesDAO {
+
+	@Override
+	public String getPage(String uuid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean containsPage(String uuid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void putPage(String uuid, String content) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deletePage(String uuid) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
