8c99483a838156ec0215dbb85ef35b6bdd4b56be
dlojea <dlojea@esei.uvigo.es>
2019-10-22 00:36:57 +0200

    Comentarios de control

--- src/es/uvigo/esei/dai/hybridserver/Launcher.java
+++ src/es/uvigo/esei/dai/hybridserver/Launcher.java
@@ -1,7 +1,11 @@
 package es.uvigo.esei.dai.hybridserver;
 
+import java.util.Properties;
+
 public class Launcher {
 	public static void main(String[] args) {
-		(new HybridServer()).start();
+		new HybridServer().start();
+		
+		//Properties props = new Properties();
 	}
 }
