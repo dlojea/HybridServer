5d51963162683e33ed4c0af8a7f3f2080d50ef71
dlojea <dlojea@esei.uvigo.es>
2019-10-22 01:15:16 +0200

    Posibilidad de usar archivo properties

--- src/es/uvigo/esei/dai/hybridserver/Launcher.java
+++ src/es/uvigo/esei/dai/hybridserver/Launcher.java
@@ -1,11 +1,34 @@
 package es.uvigo.esei.dai.hybridserver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Launcher {
 	public static void main(String[] args) {
-		new HybridServer().start();
-		
-		//Properties props = new Properties();
+		// Si no contiene archivo de propiedades crea un servidor por defecto
+		if (args.length == 0 ) {
+			
+			HybridServer server = new HybridServer();
+			server.start();
+			
+		} else if (args.length == 1) {
+			
+			Properties properties = new Properties();
+			try (InputStream inStream = new FileInputStream(args[0])) {
+				properties.load(inStream);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			HybridServer server = new HybridServer(properties);
+			server.start();
+			
+		} else {
+			System.out.println("Error: solo puede haber un parámetro");
+		}
 	}
 }
