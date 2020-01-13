6bcb4b52524c13f1aeb65828aaaeb59ae1945611
dlojea <dlojea@esei.uvigo.es>
2020-01-13 17:03:48 +0100

    Configuration.xml entra como argumento

--- src/es/uvigo/esei/dai/hybridserver/Launcher.java
+++ src/es/uvigo/esei/dai/hybridserver/Launcher.java
@@ -3,9 +3,7 @@ package es.uvigo.esei.dai.hybridserver;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
@@ -15,15 +13,19 @@ import javax.xml.validation.Validator;
 
 public class Launcher {
 	public static void main(String[] args) {
-		
-		File xml = new File("./configuration.xml");
-		
-		//Comprueba si existe el archivo xml de configuracion
-		if (xml.exists()) {
+			
+		//Si no hay ningun argumento arranca con la configuracion por defecto
+		if (args.length == 0 ) {
+			new HybridServer().start();
+			
+		//Si hay un argumento lo valida con el XSD y arranca con esa configuracion
+		} else if (args.length == 1) {
+			
 			InputStream xmlStream = null;
 			InputStream xsdStream = null;
+			
 			try {
-				xmlStream = new FileInputStream("./configuration.xml");
+				xmlStream = new FileInputStream(args[0]);
 				xsdStream = new FileInputStream("./configuration.xsd");
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
@@ -35,6 +37,7 @@ public class Launcher {
 	
 			if (validateAgainstXSD(xmlStream, xsdStream)) {
 				try {
+					File xml = new File(args[0]);
 					conf = xmlLoader.load(xml);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
@@ -44,47 +47,12 @@ public class Launcher {
 			} else {
 				System.out.println("Error: fallo al validar el archivo de configuracion");
 			}
-			
-		//Si no existe el archivo de configuracion comprueba si se le ha pasado uno de propiedades com argumento
-		} else if (args.length == 1) {
-			
-			Properties properties = new Properties();
-			try (InputStream inStream = new FileInputStream(args[0])) {
-				properties.load(inStream);
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			if (hasAllProperties(properties)) {
-				new HybridServer(properties).start();
-			} else {
-				System.out.println("Error: faltan propiedades");
-			}
-			
-		//Si no hay archivo de configuracion de propiedades comienza con valores por defecto
-		} else if (args.length == 0 ) {
-			new HybridServer().start();
-			
+		
 		//Si hay mas de un argumento da error
 		} else {
 			System.out.println("Error: solo puede haber un parámetro");
 		}
 		
-	}
-	
-	private static boolean hasAllProperties(Properties properties) {
-		String[] propertiesNames = {"numClients","port","db.url","db.user","db.password"};
-		
-		for (String property: propertiesNames) {
-			// Comprueba si no existe la propiedad o si su valor es vacío
-			if (properties.getProperty(property) == null || properties.getProperty(property).isEmpty()) {
-				
-				return false;
-			}
-		}
-		return true;
 	}
 		
 	private static boolean validateAgainstXSD(InputStream xml, InputStream xsd) {
