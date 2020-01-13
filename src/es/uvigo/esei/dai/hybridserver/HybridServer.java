648614a4a2f534ba0951be04ea9b26d3ec302a69
dlojea <dlojea@esei.uvigo.es>
2020-01-13 16:31:48 +0100

    Añadido inicio y fin de servicio

--- src/es/uvigo/esei/dai/hybridserver/HybridServer.java
+++ src/es/uvigo/esei/dai/hybridserver/HybridServer.java
@@ -8,16 +8,32 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import javax.xml.ws.Endpoint;
+
+import es.uvigo.esei.dai.hybridserver.dao.HtmlDAO;
+import es.uvigo.esei.dai.hybridserver.dao.XmlDAO;
+import es.uvigo.esei.dai.hybridserver.dao.XsdDAO;
+import es.uvigo.esei.dai.hybridserver.dao.XsltDAO;
+
 public class HybridServer {
 	private static final int SERVICE_PORT = 8888;
 	private Thread serverThread;
 	private boolean stop;
 	private ExecutorService threadPool;
+	
 	private int numClients;
 	private int port;
+	
 	private Properties properties;
 	private Configuration config;
 	
+	private HtmlDAO htmlDAO;
+	private XmlDAO xmlDAO;
+	private XsdDAO xsdDAO;
+	private XsltDAO xsltDAO;
+	private String webService;
+	
+	private Endpoint endpoint;
 
 	public HybridServer() {
 		this.numClients = 50;
@@ -38,6 +54,12 @@ public class HybridServer {
 		this.numClients = config.getNumClients();
 		this.port = config.getHttpPort();	
 		this.config = config;
+		
+		this.webService = config.getWebServiceURL();
+		this.htmlDAO = new HtmlDAO(config);
+		this.xmlDAO = new XmlDAO(config);
+		this.xsdDAO = new XsdDAO(config);
+		this.xsltDAO = new XsltDAO(config);
 	}
 
 	public int getPort() {
@@ -45,6 +67,7 @@ public class HybridServer {
 	}
 	
 	public void start() {
+		if (webService != null) endpoint = Endpoint.publish(webService, new DefaultHybridServerService(htmlDAO, xmlDAO, xsdDAO, xsltDAO));
 		this.serverThread = new Thread() {
 			@Override
 			public void run() {
@@ -56,7 +79,7 @@ public class HybridServer {
 						if (stop) break;
 						if(properties != null) threadPool.execute(new ServiceThread(socket, properties));	
 						else if (config !=null) threadPool.execute(new ServiceThread(socket, config));	
-						
+					
 							
 					}
 				} catch (IOException e) {
@@ -93,5 +116,6 @@ public class HybridServer {
 		}
 		
 		this.serverThread = null;
+		if (webService != null) endpoint.stop();
 	}
 }
